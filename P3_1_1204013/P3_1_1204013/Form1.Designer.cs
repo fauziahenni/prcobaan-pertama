805b34180b2b25a862491ff20dd61c8f52580f5f
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-11-26 19:21:50 -0800

    Add files via upload

---
+++ P3_1_1204013/P3_1_1204013/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace P3_1_1204013
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Button();
+            this.HelloButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 25);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OutputLabel.Font = new System.Drawing.Font("Trebuchet MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.OutputLabel.Location = new System.Drawing.Point(14, 30);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(264, 23);
+            this.OutputLabel.TabIndex = 1;
+            this.OutputLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.OutputLabel.UseVisualStyleBackColor = true;
+            // 
+            // HelloButton
+            // 
+            this.HelloButton.Location = new System.Drawing.Point(57, 87);
+            this.HelloButton.Name = "HelloButton";
+            this.HelloButton.Size = new System.Drawing.Size(75, 25);
+            this.HelloButton.TabIndex = 2;
+            this.HelloButton.Text = "&Say Hello";
+            this.HelloButton.UseVisualStyleBackColor = true;
+            this.HelloButton.Click += new System.EventHandler(this.HelloButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(161, 87);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 25);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "E&it";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(282, 128);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.HelloButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "From Hello";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OutputLabel;
+        private System.Windows.Forms.Button HelloButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
